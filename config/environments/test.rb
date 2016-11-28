9de5fa38067d60608906086dd1e017a585656a33
Nicole Lopez <nicole.lopez@avant.com>
2016-11-28 00:44:43 -0600

    change test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,10 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  ENV["GOOGLE_CLIENT_ID"] = 'fake'
+  ENV["GOOGLE_CLIENT_SECRET"] = 'auth'
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
